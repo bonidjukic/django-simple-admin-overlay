f90c23020a5157ec901476094643826f29f25db9
bonidjukic <boni@djukic.com.hr>
2017-03-30 15:32:57 +0200

    Prepare app for packaging with setuptools

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-admin-shortcuts',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Lightweight Django app which displays simple overlay with admin links on the frontend pages.',
+    long_description=README,
+    url='https://github.com/bonidjukic/django-admin-shortcuts',
+    author='Boni Đukić',
+    author_email='boni@djukic.com.hr',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+)
